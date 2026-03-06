054674a72a8237bad369c3e9e8ae6c631625ba75
SwastikKaushal <swastikkaushal2022008@gmail.com>
2026-03-06 19:02:06 +0530

    Added API Secret Security

--- api/index.py
+++ api/index.py
@@ -1,4 +1,5 @@
 import uvicorn
+import os
 from fastapi import FastAPI, Query
 from api.routes.clip import handle_clip_request
 from dotenv import load_dotenv
@@ -11,8 +12,12 @@ app = FastAPI()
 async def create_clip(
     user: str = Query(..., description="The user creating the clip"),
     msg: str = Query(..., description="The title of the clip and offset"),
-    channelid: str = Query(..., description="The YouTube Channel ID")
+    channelid: str = Query(..., description="The YouTube Channel ID"),
+    secret: str = Query(..., description="The secret password to protect the API")
 ):
+    if secret != os.getenv("API_SECRET"):
+        return {"message": "Error: Unauthorized API Access."}
+
     result = handle_clip_request(user, msg, channelid)
     return result
 
